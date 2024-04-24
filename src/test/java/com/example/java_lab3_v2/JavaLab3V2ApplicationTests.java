fee8746a2994cec4048ece0e27c281741d201372
kfghio <ableeve1@gmail.com>
2024-04-24 13:30:13 +0300

    Feat: created project

---
+++ src/test/java/com/example/java_lab3_v2/JavaLab3V2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_lab3_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLab3V2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
